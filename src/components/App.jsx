ee10868c13fa3c02e01dc5eeca9a5b1518e009ea
aleifericsson <alifnafili@gmail.com>
2024-04-13 12:18:43 +0100

    3d done

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import '../css/App.css'
+import Solar3D from './Solar3D';
+
+function App() {
+  return (
+    <div className='app'>
+      <div>TEST</div>
+      <Solar3D />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Solar3D.jsx
@@ -0,0 +1,9 @@
+
+export default function Solar3D() {
+
+  return (
+    <div>
+      <iframe className = "solar3d" src='https://my.spline.design/solarsystem-684ea6a4448fd6a14308a538d3dbd4b7/' frameBorder='0' width='100%' height='800px'></iframe>
+    </div>
+  );
+}
