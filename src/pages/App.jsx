1fee44225e46e576fdbfb13ef620bdac4621a3b4
Affan Imran <affanimran@Affans-MacBook-Air.local>
2024-04-13 16:56:19 +0100

    gifvgkvg

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,28 +1,28 @@
-import '../css/App.css'
-import About from './About';
-import Home from './Home';
-import Unity from './Unity';
-import Navig from '../components/Navig';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import FactMachine from './FactMachine';
+import "../css/App.css";
+import About from "./About";
+import Home from "./Home";
+import Unity from "./Unity";
+import Navig from "../components/Navig";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FactMachine from "./FactMachine";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="App">
+      <div className="cursor"></div>
       <Router>
+        <Navig />
 
-      <Navig />
-      
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/unity' element={<Unity />} />
-        <Route path='/fact-machine' element={<FactMachine />} />
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/unity" element={<Unity />} />
+          <Route path="/fact-machine" element={<FactMachine />} />
+        </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
