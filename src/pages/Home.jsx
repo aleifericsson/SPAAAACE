07c5ed56324a4ddb30464f01f3839e379cb108c6
Arnav Bisani <arnav@MacBook-Air-326.local>
2024-04-13 14:12:09 +0100

    as

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ export default function Home() {
 
     return (
       <div>
-        dffff
         <Solar3D />
       </div>
     );
--- src/pages/Unity.jsx
+++ src/pages/Unity.jsx
@@ -2,7 +2,7 @@ export default function Unity() {
 
     return (
       <div>
-        unity
+        <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/b10a1398-827b-4626-ba50-087c741be3e5"></iframe>
       </div>
     );
   }
